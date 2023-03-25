03d901c9fe7f53bae565ccc53143358ad13db5aa
JoasPraxedes <125599536+JoasPraxedes@users.noreply.github.com>
2023-03-25 17:41:08 -0300

    Add files via upload

---
+++ Atividade4/Atividade4/Atividade4/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Atividade4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.lblNum3 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum3 = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Location = new System.Drawing.Point(68, 71);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(65, 16);
+            this.lblNum1.TabIndex = 0;
+            this.lblNum1.Text = "Número 1";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Location = new System.Drawing.Point(68, 110);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(65, 16);
+            this.lblNum2.TabIndex = 1;
+            this.lblNum2.Text = "Número 2";
+            // 
+            // lblNum3
+            // 
+            this.lblNum3.AutoSize = true;
+            this.lblNum3.Location = new System.Drawing.Point(68, 149);
+            this.lblNum3.Name = "lblNum3";
+            this.lblNum3.Size = new System.Drawing.Size(65, 16);
+            this.lblNum3.TabIndex = 2;
+            this.lblNum3.Text = "Número 3";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(139, 68);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.TabIndex = 3;
+            this.txtNum1.Validated += new System.EventHandler(this.txtNum1_Validated);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(139, 107);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.TabIndex = 4;
+            this.txtNum2.Validated += new System.EventHandler(this.txtNum2_Validated);
+            // 
+            // txtNum3
+            // 
+            this.txtNum3.Location = new System.Drawing.Point(139, 143);
+            this.txtNum3.Name = "txtNum3";
+            this.txtNum3.Size = new System.Drawing.Size(100, 22);
+            this.txtNum3.TabIndex = 5;
+            this.txtNum3.Validated += new System.EventHandler(this.txtNum3_Validated);
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(71, 230);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 6;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(181, 230);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(294, 230);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtNum3);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblNum3);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label lblNum3;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum3;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Atividade4/Atividade4/Atividade4/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade4
+{
+    public partial class Form1 : Form
+    {
+        double numero1, numero2, numero3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Clear();
+            txtNum2.Clear();
+            txtNum3.Clear();
+        }
+
+        private void txtNum3_Validated(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(txtNum3.Text, out numero3))
+            {
+                MessageBox.Show("Número 3 inválido !");
+            }
+            else if (numero3 <= 0)
+            {
+                MessageBox.Show("Número 3 tem que ser maior que zero !");
+            }
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            if((numero1 < (numero2 + numero3)) && (numero1 > Math.Abs(numero2 - numero3)) &&
+                (numero2 < (numero1 + numero3)) && (numero2 > Math.Abs(numero1 - numero3)) &&
+                (numero3 < (numero2 + numero1)) && (numero3 > Math.Abs(numero2 - numero1)) && 
+                (numero1 == numero2) && (numero2 == numero3))
+            {
+                MessageBox.Show("É triângulo,e é equilátero");
+            }
+            else if ((numero1 < (numero2 + numero3)) && (numero1 > Math.Abs(numero2 - numero3)) &&
+                (numero2 < (numero1 + numero3)) && (numero2 > Math.Abs(numero1 - numero3)) &&
+                (numero3 < (numero2 + numero1)) && (numero3 > Math.Abs(numero2 - numero1)) &&
+                (numero1 == numero2) || (numero1 == numero3) || (numero2 == numero3))
+            {
+                MessageBox.Show("É triângulo,e é isósceles");
+            }
+            else
+            {
+                MessageBox.Show("É triângulo,e é escaleno");
+            }
+        }
+
+        private void txtNum1_Validated(object sender, EventArgs e)
+        {
+            if(!Double.TryParse(txtNum1.Text, out numero1))
+            {
+                MessageBox.Show("Número 1 inválido !");
+            }
+            else if(numero1 <= 0)
+            {
+                MessageBox.Show("Número 1 tem que ser maior que zero !");
+            }
+        }
+
+        private void txtNum2_Validated(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(txtNum2.Text, out numero2))
+            {
+                MessageBox.Show("Número 2 inválido !");
+            }
+            else if (numero2 <= 0)
+            {
+                MessageBox.Show("Número 2 tem que ser maior que zero !");
+            }
+        }
+    }
+}
